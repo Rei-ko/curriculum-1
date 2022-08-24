14ec89f0ad3c06b7c1b717e785970b971822363a
Rei-ko <108601812+Rei-ko@users.noreply.github.com>
2022-08-24 01:50:23 -0600

    課題

--- 2-17/index1.php
+++ 2-17/index1.php
@@ -1,18 +1,14 @@
 <?php
 date_default_timezone_set('Asia/Tokyo');
 $time = date('H');
+echo "今{$time}時台です";
+echo '<br>';
 
 if (5 <= $time && $time <= 11 ){
-    echo "今{$time}時台です";
-    echo '<br>';
     echo "おはようございます";
 } elseif (12 <= $time && $time <= 17){
-    echo "今{$time}時台です";
-    echo '<br>';
     echo "こんにちは";
 } else {
-    echo "今{$time}時台です";
-    echo '<br>';
     echo "こんばんは";
 }
 ?>
